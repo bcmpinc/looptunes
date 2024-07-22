601493e4cf2cea2c332699271e372349580bf253
Bauke Conijn <bcmpinc@gmail.com>
2024-07-22 07:20:32 +0200

    It zooms

--- src/pancamera.rs
+++ src/pancamera.rs
@@ -1,4 +1,4 @@
-use bevy::input::mouse::MouseMotion;
+use bevy::input::mouse::{MouseMotion, MouseWheel};
 use bevy::window::{CursorGrabMode, PrimaryWindow};
 use bevy::transform::components::Transform;
 use bevy::prelude::*;
@@ -7,11 +7,13 @@ use bevy::app::{App, Plugin, Update};
 pub struct PanCamera;
 
 const DRAG_BUTTON : MouseButton = MouseButton::Left;
+const ZOOM_SENSITIVITY: f32 = -0.2;
 
 impl Plugin for PanCamera {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, camera_movement);
         app.add_systems(Update, cursor_grab);
+        app.add_systems(Update, camera_zoom);
     }
 }
 
@@ -22,10 +24,23 @@ fn camera_movement(
 ) {
     if buttons.pressed(DRAG_BUTTON) {
         let mut transform = query.single_mut();
+        let scale = transform.scale.x;
         for event in motion.read() {
-            transform.translation.x -= event.delta.x;
-            transform.translation.y += event.delta.y;
+            transform.translation.x -= event.delta.x * scale;
+            transform.translation.y += event.delta.y * scale;
         }
+    }
+}
+
+fn camera_zoom(
+    mut query: Query<&mut Transform, With<Camera2d>>,
+    mut scroll: EventReader<MouseWheel>,
+) {
+    let mut transform = query.single_mut();
+    for event in scroll.read() {
+        let scale_change = f32::powf(2.0, event.y * ZOOM_SENSITIVITY);
+        transform.scale *= Vec3::new(scale_change, scale_change, 1.0);
+        transform.scale = Vec3::max(transform.scale, Vec3::new(0.1, 0.1, 1.0));
     }
 }
 
