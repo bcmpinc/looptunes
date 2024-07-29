2769f966cfaeeca61b6cce75ce6cfb2314fc1cfb
Bauke Conijn <bcmpinc@gmail.com>
2024-07-29 07:12:56 +0200

    copy paste proof of concept

--- src/archiving.rs
+++ src/archiving.rs
@@ -11,7 +11,7 @@ impl Plugin for ArchivingPlugin {
         let x = ctx.set_contents("This is a test".into());
         println!("Res: {:?}", x);
 
-        let document = web_sys::window().unwrap().document().unwrap();
+        let window = web_sys::window().unwrap();
 
         web_sys::console::log_1(&format!("Initializing clipboard!").into());
 
@@ -19,17 +19,31 @@ impl Plugin for ArchivingPlugin {
         let paste_closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::ClipboardEvent| {
             web_sys::console::log_1(&format!("Pasting").into());
             if let Some(clipboard_data) = event.clipboard_data() {
-                if let Ok(pasted_text) = clipboard_data.get_data("text") {
+                if let Ok(pasted_text) = clipboard_data.get_data("text/plain") {
                     web_sys::console::log_1(&format!("Pasted text: {}", pasted_text).into());
                 }
             }
         });
 
-        // Get the body element and attach the event listener.
-        let body = document.body().unwrap();
-        _ = body.add_event_listener_with_callback("paste", paste_closure.as_ref().unchecked_ref());
+        _ = window.add_event_listener_with_callback("paste", paste_closure.as_ref().unchecked_ref());
 
         // Prevent the closure from being dropped.
-        paste_closure.forget();        
+        paste_closure.forget();
+
+        // Set up a copy event listener.
+        let copy_closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::ClipboardEvent| {
+            web_sys::console::log_1(&format!("Copying").into());
+            if let Some(clipboard_data) = event.clipboard_data() {
+                _ = clipboard_data.set_data("text/plain", "Web clipboard content from rust").unwrap();
+                web_sys::console::log_1(&"Copy event handled!".into());
+            }
+            event.prevent_default();
+        });
+
+        _ = window.add_event_listener_with_callback("copy", copy_closure.as_ref().unchecked_ref());
+
+        // Prevent the closure from being dropped.
+        copy_closure.forget();
+
     }
 }
